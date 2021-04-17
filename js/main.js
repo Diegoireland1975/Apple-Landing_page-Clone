0c45ad533c443a15bf3bea008fea02186a960480
Diego Raffa <diego170975@gmail.com>
2021-04-17 12:01:01 +0100

    added footer

--- js/main.js
+++ js/main.js
@@ -6,3 +6,13 @@ navToggler.addEventListener("click", () => {
   navLinks.classList.toggle("show");
   navToggler.classList.toggle("rotate");
 });
+
+// collapsible items
+const collHeaders = document.querySelectorAll(".collapsible-header");
+const collContents = document.querySelectorAll(".collapsible-content");
+
+collHeaders.forEach((collHeader) => {
+  collHeader.addEventListener("click", () => {
+    collHeader.classList.toggle("show");
+  });
+});
